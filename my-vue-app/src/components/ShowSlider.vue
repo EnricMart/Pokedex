d2a0a304c38b4dff33a250bb6eb40ec4dcdd5f27
EnricMart <131441239+EnricMart@users.noreply.github.com>
2024-06-10 22:15:02 +0200

    Update ShowSlider.vue

--- my-vue-app/src/components/ShowSlider.vue
+++ my-vue-app/src/components/ShowSlider.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="slider-container">
-    <input type="range" :min="minValue" :max="maxValue" v-model="startValue" @input="updateRange">
-    <input type="range" :min="minValue" :max="maxValue" v-model="endValue" @input="updateRange">
+    <input type="range" :min="minValue" :max="midValue" v-model="startValue" @input="updateStartValue">
+    <input type="range" :min="midValuePlusOne" :max="maxValue" v-model="endValue" @input="updateEndValue">
   </div>
 </template>
 
@@ -12,13 +12,21 @@ export default {
       startValue: 1,
       endValue: 151,
       minValue: 1,
+      midValue: 75,
+      midValuePlusOne: 76,
       maxValue: 151
     };
   },
   methods: {
-    updateRange() {
-      if (this.startValue > this.endValue) {
-        this.endValue = this.startValue;
+    updateStartValue() {
+      if (this.startValue > this.midValue) {
+        this.startValue = this.midValue;
+      }
+      this.$emit('update-range', { start: this.startValue, end: this.endValue });
+    },
+    updateEndValue() {
+      if (this.endValue < this.midValuePlusOne) {
+        this.endValue = this.midValuePlusOne;
       }
       this.$emit('update-range', { start: this.startValue, end: this.endValue });
     }
